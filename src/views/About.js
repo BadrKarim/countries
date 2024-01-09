8ad4f3252dae1dcd70ab1a1b3dfa7cda1642b8a0
BadrKarim <karim-badr@hotmail.fr>
2024-01-09 01:16:34 +0100

    second commit

--- src/views/About.js
+++ src/views/About.js
@@ -3,7 +3,12 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            
+            <h1>A propos</h1>
+            <br/>
+            <p>
+                lorem400
+                
+            </p>
         </div>
     );
 };
