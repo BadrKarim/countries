43d628c708d905a0692530778560407e4217735a
BadrKarim <karim-badr@hotmail.fr>
2024-01-09 03:00:39 +0100

    site vitrine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './views/Home';
+import About from './views/About';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        {/* le path * fonctionne si jamais l'url ne corespond à rien de déclaré au dessus */}
+        <Route path='*' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 };
 
--- src/views/About.js
+++ src/views/About.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
 
 const About = () => {
     return (
         <div>
+            <Logo />
+            <Navigation/>
             <h1>A propos</h1>
+
             <br/>
+
             <p>
-                lorem400
-                
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum eveniet, harum totam a, perspiciatis sint, sapiente architecto eum dolore adipisci fuga dolorem numquam quibusdam dolorum? Repellat, delectus non similique voluptatum laudantium excepturi, vel enim modi porro sed quisquam earum doloremque ipsa temporibus perferendis quod asperiores suscipit aliquid ratione quidem nihil recusandae? Illo soluta quas sunt? Voluptate similique quis doloremque natus voluptates minus possimus vitae voluptatem optio illo temporibus esse necessitatibus non amet alias cum quasi, nulla reprehenderit. Facere temporibus praesentium nesciunt. Ab nobis rerum molestiae debitis porro consequatur laboriosam quas, necessitatibus facilis est obcaecati incidunt eum, recusandae cupiditate veritatis soluta.
+            </p>
+
+            <br />
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis illum, voluptate animi mollitia id nulla tempora aperiam consequatur dolor nobis eligendi beatae fugiat voluptatem eveniet recusandae alias dolores expedita ad doloribus suscipit soluta debitis? Harum, soluta delectus voluptates quas voluptate fugit doloremque quae repellendus dolor error minima deleniti nemo voluptatibus nesciunt unde, provident suscipit reprehenderit, aut eaque quaerat vitae facilis tempore possimus nulla. Obcaecati et suscipit adipisci ratione, placeat corrupti debitis eligendi ea quae quod voluptates, consequuntur excepturi ad! Exercitationem labore minus, doloribus ab nihil aut fuga id eligendi reprehenderit. Velit magni vel modi quis, non at dicta, ut dolor asperiores vero optio perspiciatis, exercitationem aliquid doloremque harum est in! Consequuntur neque voluptatem dolores eos! Numquam, iusto delectus vitae ipsum beatae cupiditate, non amet eos ut, ex similique doloremque odit laboriosam. Possimus, perferendis! Dolorem quis quia voluptates nulla mollitia sapiente ullam, repellendus ipsam, nostrum nemo, voluptas cupiditate dolore enim reiciendis earum eos! Dolor odio sit optio fugit ex eius obcaecati maiores enim amet voluptatem. Dolore, impedit ipsum eveniet quos, porro quaerat qui fugit magnam eius, nam excepturi pariatur. Pariatur aspernatur maiores excepturi quis eum officiis. Ea, iure maxime sequi temporibus ducimus sapiente, fugiat dicta laudantium fugit quidem impedit praesentium sint.
             </p>
         </div>
     );
